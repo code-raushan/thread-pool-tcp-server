080678f5b3a832cdcbe591be138f283192431c08
code-raushan <code-raushan@users.noreply.github.com>
2024-07-20 19:30:24 +0530

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/code-raushan/thread-pool-tcp-server
+
+go 1.22.5
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+// processing state enums
+const (
+	WAIT_FOR_MSG = iota
+	IN_MSG
+)
+
+func main(){
+	
+}
