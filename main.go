99da8f92a2253f6b135801d252281d28e81c2de0
code-raushan <code-raushan@users.noreply.github.com>
2024-07-20 19:34:21 +0530

    add: tcp server listener

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+)
+
 // processing state enums
 const (
 	WAIT_FOR_MSG = iota
@@ -7,5 +14,14 @@ const (
 )
 
 func main(){
-	
+	port := flag.Int("port", 9090, "Specify the port for the TCP server. (default is 9090)")
+	numOfWorkers := flag.Int("workers", 2, "Specify the number of workers in the thread pool. (default is 2)")
+
+	flag.Parse()
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d",port))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting the server: %v", err)
+		os.Exit(1)
+	}
 }
